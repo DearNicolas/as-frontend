c346b346b3a306ebff40c3c94998446312953c80
nicolas <nicolas.dearmini@gmail.com>
2024-03-04 15:55:12 -0300

    final project

--- src/components/site/SearchReveal.tsx
+++ src/components/site/SearchReveal.tsx
@@ -9,7 +9,8 @@ export const SearchReveal = ({ results }: Props) => {
             <p className="text-3xl">{results.person.name}</p>
             <p className="text-2xl my-3">parabéns, você tirou:</p>
             <p className="text-4xl bg-blue-800 my-5 px-5 py-20 rounded-lg
-            border-2 border-dashed border-blue-300">{results.personMatched.name}</p>
+            border-2 border-dashed border-blue-300 ">{results.personMatched.name}</p>
+            <p className="text-2xl">Agora se divirta e escolha um presente para essa pessoa.</p>
         </div>
     )
 }
