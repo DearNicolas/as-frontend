fa6b4c3322e8cb2a53849f8bcabb066b57225089
nicolas <nicolas.dearmini@gmail.com>
2024-03-06 17:26:13 -0300

    css parcialmente alterado

--- src/components/admin/Modal.tsx
+++ src/components/admin/Modal.tsx
@@ -9,8 +9,8 @@ export const Modal = ({ onClose, children }: Props) => {
     return (
         <div className="fixed left-0 top-0 right-0 bottom-0 bg-black/70 flex flex-col items-center overflow-y-auto ">
             <div className="w-full max-w-xl my-3 ">
-                <div onClick={onClose} className=" w-8 h-8 rounded-full bg-gradient-to-r from-black via-gray-400 to-black text-white text-lg
-                flex justify-center items-center cursor-pointer hover:from-black hover:via-white hover:to-black">X</div>
+                <div onClick={onClose} className=" w-8 h-8 rounded-full bg-black text-white text-lg
+                flex justify-center items-center cursor-pointer hover:bg-gray-400">X</div>
             </div>
             <div className="bg-black w-full max-w-xl p-4 rounded-md mb-5">{children}</div>
         </div>
